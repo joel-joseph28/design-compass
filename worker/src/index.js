7ec32b0e57209dd9590b43ea5a097a282025f974
decoded-cipher <arjun12345krishna@gmail.com>
2024-10-03 00:01:07 +0530

    Cloudflare R2 upload worker

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,41 +1,49 @@
-import { nanoid } from 'nanoid';
-
 export default {
-	async fetch(request, env) {
+    async fetch(request, env, ctx) {
+        if (request.method !== 'POST') {
+            return jsonResponse({ error: 'Method not allowed' }, 405);
+        }
 
-		if (request.method !== 'POST') {
-			return new Response('Method not allowed', { status: 405 });
-		}
+        try {
+            const formData = await request.formData();
+            const imageFile = formData.get('image');
 
-		try {
-			const formData = await request.formData();
-			const file = formData.get('file');
+            if (!imageFile) {
+                return jsonResponse({ error: 'No image file uploaded' }, 400);
+            }
 
-			if (!file) {
-				return new Response('No file uploaded', { status: 400 });
-			}
+            if (!imageFile.type.startsWith('image/')) {
+                return jsonResponse({ error: 'Uploaded file is not an image' }, 400);
+            }
 
-			if (!file.type.startsWith('image/')) {
-				return new Response('Uploaded file is not an image', { status: 400 });
-			}
+			const imageExt = getImageExt(imageFile.name);
+            const uniqueFilename = `${Date.now()}${imageExt}`;
 
-			const filename = `${nanoid()}.${getFileExtension(file.name)}`;
+            await env.MY_BUCKET.put(uniqueFilename, imageFile.stream(), {
+                httpMetadata: {
+                    contentType: imageFile.type,
+                },
+            });
 
-			await env.MY_BUCKET.put(filename, file);
+			const imageUrl = `${env.R2_PUBLIC_DOMAIN}/${uniqueFilename}`;
 
-			const publicUrl = `https://${env.R2_PUBLIC_DOMAIN}/${filename}`;
-
-			return new Response(JSON.stringify({ url: publicUrl }), {
-				headers: { 'Content-Type': 'application/json' },
-			});
-			
-		} catch (error) {
-			return new Response(`Error: ${error.message}`, { status: 500 });
-		}
-
-	},
+            return jsonResponse({ 
+                message: 'Image uploaded successfully', 
+                url: imageUrl 
+            });
+        } catch (err) {
+            return jsonResponse({ error: `Error uploading image: ${err.message}` }, 500);
+        }
+    }
 };
 
-function getFileExtension(filename) {
-	return filename.split('.').pop();
+function jsonResponse(data, status = 200) {
+    return new Response(JSON.stringify(data), {
+        status,
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
+
+function getImageExt(imgName) {
+	return imgName.slice((imgName.lastIndexOf('.') - 1 >>> 0) + 1);
 }
